2b9dbe38762eeb477eb6325cb0819b23496a6fe3
Ricardo Souza <ricksouz96@gmail.com>
2024-10-04 21:49:57 -0300

    Implements the Description method, that returns a message with the time and date scheduled

--- BookingUpForBeauty.cs
+++ BookingUpForBeauty.cs
@@ -7,11 +7,8 @@ static class Appointment
     public static bool HasPassed(DateTime appointmentDate) => DateTime.Now > appointmentDate;
 
     public static bool IsAfternoonAppointment(DateTime appointmentDate) => appointmentDate.TimeOfDay.TotalHours >= 12;
-        
-    public static string Description(DateTime appointmentDate)
-    {
-        throw new NotImplementedException("Please implement the (static) Appointment.Description() method");
-    }
+
+    public static string Description(DateTime appointmentDate) => $"You have an appointment on {appointmentDate.ToString()}.";
 
     public static DateTime AnniversaryDate()
     {
